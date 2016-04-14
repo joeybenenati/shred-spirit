5179f68d87cb81a295952d15fdd4ae226e1bf48a
Joey Benenati <joey.benenati@gmail.com>
2016-04-14 13:03:43 -0700

    header

--- client/main.jsx
+++ client/main.jsx
@@ -4,7 +4,8 @@ import { render } from 'react-dom';
 import { Router, Route, Link, browserHistory } from 'react-router'
  
 import '../imports/startup/accounts-config.js'; 
-import App from '../imports/ui/App.jsx';
+import App_old from '../imports/ui/App_old.jsx';
+import App from '../imports/ui/App.jsx'
  
 Meteor.startup(() => {
   render((
---
+++ imports/ui/landing/Header.jsx
@@ -0,0 +1,17 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <header className='intro-header'>
+        <div className='container'>
+          <div className='col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1'>
+            <div className='site-heading'>
+              <h1>shredded spirit</h1>
+            </div>
+          </div>
+        </div>
+      </header>
+    )
+  }
+}
