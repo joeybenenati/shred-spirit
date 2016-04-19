0ff90c61701169736f936aef7a1bc08b8240a4f3
Joey Benenati <joey.benenati@gmail.com>
2016-04-18 17:56:07 -0700

    navbar

--- imports/ui/user/User.jsx
+++ imports/ui/user/User.jsx
@@ -1,17 +1,15 @@
 import React, { Component, PropTypes } from 'react';
 import { Meteor } from 'meteor/meteor'
+import NavBar from '../Navbar.jsx'
 
 export default User = React.createClass({
-  getInitialState() {
-    return {
-      profile: Meteor.user().profile
-    }
-  },
 
   render() {
-    var firstName = this.state.profile.first_name
     return (
-      <h1>Hi, {firstName}!</h1>
+      <div>
+        <NavBar/>
+        <h1>Hi, {/*this.state.firstName*/}!</h1>
+      </div>
     )
   }
 })
